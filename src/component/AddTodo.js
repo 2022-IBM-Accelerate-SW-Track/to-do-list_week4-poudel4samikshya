dd37448f5ab64455537183ba5537461a9848e06d
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2022-07-04 18:04:30 -0400

    finished lab 4

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
+
+
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -16,7 +19,7 @@ class AddTodo extends Component {
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
   // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
   // into the text field.
-  handleChange = (event) => {
+handleChange = (event) => {
     this.setState({
       content: event.target.value,
       date: Date().toLocaleString('en-US')
@@ -39,7 +42,27 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
+
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+     };
+
+     Axios({
+      method: "POST",
+      url: "http://localhost:8080/add/item",
+      data: {jsonObject},
+      headers: {
+         "Content-Type": "application/json"
+      }
+   }).then(res => {
+      console.log(res.data.message);
+   });
+
+
+    this.props.addTodo(this.state);
       this.setState({
         content: "",
         date: "",
@@ -47,6 +70,7 @@ class AddTodo extends Component {
       });
     }
   };
+  
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
